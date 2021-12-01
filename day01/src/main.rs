3c560d1dd89715151ee4321c658c0dc30c9f01db
Anthony Cramp <anthony.cramp@gmail.com>
2021-12-01 22:04:19 +1030

    Pulled input parsing out to its own function since it will be used by part1 and part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -6,13 +6,17 @@ fn main() {
     println!("Day NN Part 2: {:?}", part2(INPUT));
 }
 
-// replace return type as required by the problem
-fn part1(input: &str) -> i32 {
-    let depths: Vec<i32> = input
+fn parse_input(input: &str) -> Vec<i32> {
+    input
         .lines()
         .map(|s| s.trim())
         .map(|s| s.parse::<i32>().unwrap())
-        .collect();
+        .collect()
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    let depths = parse_input(input);
     let mut depths_iter = depths.iter();
     let mut prev = depths_iter.next();
     let mut next = depths_iter.next();
@@ -59,16 +63,10 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
+        let test_cases = [TestCase {
+            input: TEST_INPUT,
+            expected: 5,
+        }];
         for TestCase { input, expected } in test_cases.iter() {
             assert_eq!(part2(*input), *expected);
         }
