7bc1826a06ede6b3ca58ed968146e6b94a9e1f8d
Anthony Cramp <anthony.cramp@gmail.com>
2021-12-09 03:25:49 +1030

    Solution for Day 08 Part 1

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -7,7 +7,25 @@ fn main() {
 
 // replace return type as required by the problem
 fn part1(input: &str) -> i32 {
-    0
+    let lines = input.lines();
+    let mut counts: Vec<i32> = vec![0; 10];
+
+    for line in lines {
+        let mut input_output = line.split(" | ");
+        let output = input_output.nth(1).unwrap();
+        let output_seven_segments = output.split_ascii_whitespace();
+        for output_seven_segment in output_seven_segments {
+            match output_seven_segment.len() {
+                2 => counts[1] += 1,
+                3 => counts[7] += 1,
+                4 => counts[4] += 1,
+                7 => counts[8] += 1,
+                _ => (),
+            }
+        }
+    }
+
+    counts.iter().sum::<i32>() as i32
 }
 
 // replace return type as required by the problem
@@ -36,7 +54,7 @@ mod tests {
     fn test_part2() {
         let test_cases = [TestCase {
             input: TEST_INPUT,
-            expected: 123,
+            expected: 61229,
         }];
         for TestCase { input, expected } in test_cases.iter() {
             assert_eq!(part2(*input), *expected);
